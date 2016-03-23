c52839dab677d986daf928f7ebd94faa09355b69
이진우 <came1230@naver.com>
2016-03-23 18:50:30 +0900

    stage 내용 추가
    - 라운드 클래스 추가

--- ThreeKnights/ThreeKnights/Stage.h
+++ ThreeKnights/ThreeKnights/Stage.h
@@ -1,4 +1,18 @@
 #pragma once
+#include "Character.h"
+class CRound
+{
+private:
+
+	std::list<CCharacter> m_enemy_Character;	// 적 캐릭터 리스트
+	std::vector<int> m_enemy_currentHp;			// 적 현재 체력 (벡터) 
+	std::vector<int> m_player_currentHp;		// 플레이어 현재 체력 (벡터)
+
+public:
+	bool ExitRound();							//라운드 종료조건
+};
+
+
 class CStage
 {
 private:
@@ -7,12 +21,11 @@ private:
 	enum class difficulty { easy, normal, hard };
 
 	//데이터 멤버
-	int m_MaxRound;							// 한 스테이지당 총 라운드
-	std::string m_stage_name;				// 스테이지 이름
-	std::string m_stage_explanation;		// 스테이지 설명
-	difficulty m_difficulty;				// 스테이지 난이도 (1:쉬움 2:보통 3:어려움)
-	
-	
+	int m_MaxRound;								// 한 스테이지당 총 라운드
+	std::string m_stage_name;					// 스테이지 이름
+	std::string m_stage_explanation;			// 스테이지 설명
+	difficulty m_difficulty;					// 스테이지 난이도 (1:쉬움 2:보통 3:어려움)
+	std::list<CRound> m_round;					// 라운드 정보
 
 public:
 
