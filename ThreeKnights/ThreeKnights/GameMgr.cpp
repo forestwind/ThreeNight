92dfa615750fd33751228f319a94377634bbddce
eunhaO <eunha0174@gmail.com>
2016-03-25 18:14:54 +0900

    게임 매니져 씬추가
    GameMgr: 게임 매니져에서 Init()함수에서 Scene들을 추가
    ThreeKnights: 싱글톤 게임매니져 Init() 테스트 호출

--- ThreeKnights/ThreeKnights/GameMgr.cpp
+++ ThreeKnights/ThreeKnights/GameMgr.cpp
@@ -1,6 +1,10 @@
 #include "StdAfx.h"
 #include "GameMgr.h"
+#include "PlayerScene.h"
+#include "ReadyScene.h"
+#include "ResultScene.h"
 
+using namespace std;
 
 CGameMgr::CGameMgr(void)
 {
@@ -13,6 +17,22 @@ CGameMgr::~CGameMgr(void)
 
 void CGameMgr::Init()
 {
+	// ¾À Ãß°¡ 
+#ifndef DEBUG
+	cout << "CGameMgr::init()" << endl; 
+#endif
+
+	// ·¹µð¾À Ãß°¡
+	CScene* temp_scene = new CReadyScene();
+	m_vScenes.push_front(*temp_scene);
+
+	// ÇÃ·¹ÀÌ¾À Ãß°¡
+	temp_scene = new CPlayerScene();
+	m_vScenes.push_front(*temp_scene);
+
+	// °á°ú¾À Ãß°¡
+	temp_scene = new CResultScene();
+	m_vScenes.push_front(*temp_scene);
 
 }
 
