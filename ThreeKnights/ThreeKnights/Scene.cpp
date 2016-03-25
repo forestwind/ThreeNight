2cf2ca4efea150ba4f9df3120e8c01cdcadab8b2
JungMin-Cho <monamigreen1@gmail.com>
2016-03-25 18:00:49 +0900

    Scene,Character -> 수정

--- ThreeKnights/ThreeKnights/Scene.cpp
+++ ThreeKnights/ThreeKnights/Scene.cpp
@@ -1,14 +1,41 @@
 #include "StdAfx.h"
 #include "Scene.h"
 
-
 CScene::CScene(void)
 {
 }
 
+CScene::CScene(std::string input){
+	m_name = input;
+}
 
 CScene::~CScene(void)
 {
+}
+
+void CScene::Set_Name(std::string input){
+	m_name = input;
+}
+
+void CScene::Set_Name(CScene::State state){
+	switch (state)
+	{
+	case State::ready:
+		m_name = "ready scene";
+		break;
+	case State::play:
+		m_name = "play scene";
+		break;
+	case State::result:
+		m_name = "result scene";
+		break;
+	default:
+		break;
+	}
+}
+
+std::string CScene::Get_Name(){
+	return m_name;
 }
 
 void CScene::Init(void){
--- ThreeKnights/ThreeKnights/Scene.h
+++ ThreeKnights/ThreeKnights/Scene.h
@@ -1,11 +1,18 @@
 #pragma once
 class CScene
 {
+private:
+	std::string m_name;
 public:
+	enum class State{ready,play,result};
 	CScene(void);
-	virtual ~CScene(void) = 0;
+	virtual ~CScene(void);
 	virtual void Init();
 	virtual void Update();
 	virtual void Render();
+	CScene(std::string input);
+	void Set_Name(std::string input);
+	void Set_Name(CScene::State state);
+	std::string Get_Name();
 };
 
